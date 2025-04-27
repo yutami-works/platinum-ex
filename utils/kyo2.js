a95d5a09b9615b75f7451d334601f5f686bc2eec
yutami <sabacan623@gmail.com>
2025-04-27 19:39:19 +0900

    rawのサイズを変更するロジックを追加

--- utils/kyo2.js
+++ utils/kyo2.js
@@ -4,7 +4,7 @@ const checkRawOrgRszImages = async (rawImages, originalImages, resizeImages) => 
 
   const rawOrgRszImages = [];
   for (let i = 0; i < rawImages.length; i++) {
-    const raw = rawImages[i];
+    let raw = rawImages[i];
     const original = originalImages[i];
     const resize = resizeImages[i];
 
@@ -14,6 +14,12 @@ const checkRawOrgRszImages = async (rawImages, originalImages, resizeImages) => 
     // 全ての項目が空欄の登録はスキップ
     if (!raw && !originalUrl && !resizeUrl) {
       continue
+    }
+
+    // 最高画質
+    if (raw && raw.startsWith('https://prod-pairs-jp-icon-cdn.pairs.lv/')) {
+      const [baseUrl] = raw.split('?');
+      raw = `${baseUrl}?v=1&width=1280&height=1280`;
     }
 
     // original が空なら raw をアップロード
