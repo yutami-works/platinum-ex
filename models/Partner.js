d53473c1f32c743742e6a5b409b6ab80bda77132
yutami <sabacan623@gmail.com>
2025-05-24 05:21:37 +0900

    スキーマを修正

--- models/Partner.js
+++ models/Partner.js
@@ -1,21 +1,31 @@
 const mongoose = require('mongoose');
 
 const partnerSchema = new mongoose.Schema({
-  hash: { type: String, required: true },
+  hash: { type: String, required: true , unique: true },
+  partnerNumber: String,
+  join: { type: Date },
   name: String,
-  birth: String,
-  tall: String,
+  birth: { type: Date },
+  height: { type: Number},
   figure: String,
   job: String,
   from: String,
   live: String,
-  connect: String,
-  quit: String,
-  like: Number,
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  }
+  blood: String,
+  horoscope: String,
+  quit: { type: Boolean, default: false },
+  private: { type: Boolean, default: false },
+  negotiate: { type: Boolean, default: false },
+  connect: { type: Boolean, default: false },
+  like: { type: Number, default: 0 },
+  images: [
+    {
+      display: { type: Boolean, default: false },
+      raw: String,
+      original: String,
+      resize: String
+    }
+  ]
 }, { collection: 'partners', timestamps: true });
 
 module.exports = mongoose.model('Partner', partnerSchema);
