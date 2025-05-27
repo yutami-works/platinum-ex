f079cb502a238524dba25952ec4ea970c2622ccf
yutami <sabacan623@gmail.com>
2025-05-27 22:39:59 +0900

    初期登録画像を修正

--- tools/insert-puser.js
+++ tools/insert-puser.js
@@ -11,24 +11,24 @@ const hash = process.argv[2];
 (async () => {
   console.log('api');
   const userInfo = await getUserInfo(hash);
-  //console.log('id：', userInfo.id);
-  //console.log('user_id：', userInfo.user_id);
+  console.log('id：', userInfo.id);
+  console.log('user_id：', userInfo.user_id);
   console.log('age：', userInfo.age);
-  //console.log('nickname：', userInfo.nickname);
-  //console.log('height：', userInfo.height);
-  //console.log('residence_state：', userInfo.residence_state);
-  //console.log('home_state：', userInfo.home_state);
-  //console.log('job：', userInfo.job);
-  //console.log('body_build：', userInfo.body_build);
-  //console.log('blood_type：', userInfo.blood_type);
-  //console.log('horoscope：', userInfo.horoscope);
-  //console.log('account_status：', userInfo.account_status);
+  console.log('nickname：', userInfo.nickname);
+  console.log('height：', userInfo.height);
+  console.log('residence_state：', userInfo.residence_state);
+  console.log('home_state：', userInfo.home_state);
+  console.log('job：', userInfo.job);
+  console.log('body_build：', userInfo.body_build);
+  console.log('blood_type：', userInfo.blood_type);
+  console.log('horoscope：', userInfo.horoscope);
+  console.log('account_status：', userInfo.account_status);
   console.log('got_like：', userInfo.got_like);
   console.log('since_last_login_seconds：', userInfo.since_last_login_seconds);
-  //console.log('created_at：', userInfo.created_at);
-  //console.log('like_from_me：', userInfo.states.like_from_me); // 1:on 2:none
-  //console.log('match：', userInfo.states.match); // 1:match 2:none
-  //console.log('private_mode：', userInfo.states.private_mode);
+  console.log('created_at：', userInfo.created_at);
+  console.log('like_from_me：', userInfo.states.like_from_me); // 1:on 2:none
+  console.log('match：', userInfo.states.match); // 1:match 2:none
+  console.log('private_mode：', userInfo.states.private_mode);
   console.log('is_banned：', userInfo.states.is_banned);
   console.log('ignore_like_from_me_status：', userInfo.states.ignore_like_from_me_status);
   for (const img of userInfo.images ) {
@@ -39,6 +39,7 @@ const hash = process.argv[2];
     const today = new Date();
     const birthYear = today.getFullYear() - age;
     const birth = new Date(birthYear, today.getMonth(), today.getDate());
+    return birth;
   }
 
   const partner = await Partner.findOne({ hash });
@@ -68,8 +69,8 @@ const hash = process.argv[2];
         like: 0,
         images: userInfo.images?.map(img => ({
           raw: img.url + '&width=1280&height=1280',
-          original: '',
-          resize: '',
+          original: img.url + '&width=1280&height=1280',
+          resize: img.url + '&width=1280&height=1280',
           display: false
         })) || []
       });
